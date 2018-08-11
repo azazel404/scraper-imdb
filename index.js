053f1ee5726f17cd10affa20b928cf2137207092
hendra <gunawan4041@gmail.com>
2018-08-11 12:49:41 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const scraper = require('./scrapper');
+
+
+const app = express();
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Scraping is Fun!'
+    });
+});
+
+
+app.get('/search/:title', (req, res) => {
+    scraper
+        .searchMovies(req.params.title)
+        .then(movies => {
+            res.json(movies);
+        });
+});
+
+app.get('/movie/:imdbID', (req, res) => {
+    scraper
+        .getMovie(req.params.imdbID)
+        .then(movie => {
+            res.json(movie);
+        });
+});
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
